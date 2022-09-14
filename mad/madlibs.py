4afedf36095fae657e247aeaebd081173df77482
alessandra1408 <alessandraoitnb@gmail.com>
2022-09-14 17:26:00 -0300

    projetc 2 - random number

---
+++ mad/madlibs.py
@@ -0,0 +1,7 @@
+youtuber = input(str("\nInsira o nome do seu canal: "))
+
+#ways to concat
+
+print('Subscribe to ' + youtuber)
+print('Subscribe to {}'.format(youtuber))
+print(f'Subscribe to {youtuber}')
